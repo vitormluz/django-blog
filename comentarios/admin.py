8507cab2f21fe86ab64828599f9fc0a59fc64ef9
vitormluz <vitormdluzz2003@outlook.com>
2022-09-12 12:06:02 -0300

    finish_the_project

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Comentario
 
 
-admin.site.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_comentario', 'email_comentario',
+                    'post_comentario', 'data_comentario',
+                    'publicado_comentario')
+    list_editable = ('publicado_comentario',)
+    list_display_links = ('id', 'nome_comentario', 'email_comentario',)
+
+
+admin.site.register(Comentario, ComentarioAdmin)
